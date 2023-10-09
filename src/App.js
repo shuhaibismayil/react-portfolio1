6ccce59c1925323a364f2438ad3f2d1e6f011a4e
shuhaibismayil <shuhaibismayil0070@gmail.com>
2023-10-09 16:21:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import Information from "./components/Information";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Sociallinks from "./components/Sociallinks";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <Navbar/>
+  <Home/>
+  <About/>
+  <Portfolio/>
+  <Experience/>
+  <Information/>
+
+  <Sociallinks/>
+</div>
   );
 }
 
